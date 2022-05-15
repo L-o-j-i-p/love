335d5ca263eddc978cf0e7b2fab18c93c5a2190a
L-o-j-i-p <99147215+L-o-j-i-p@users.noreply.github.com>
2022-05-15 12:51:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function myFunction() {
+         document.getElementById("demo").innerHTML = "ня❤️❤️❤️";
+}
+
+function mFunction() {
+    document.getElementById("de").innerHTML = "люблю тебя моя жена будущая❤️❤️❤️";
+}
+
+function Function() {
+    document.getElementById("dem").innerHTML = "выйдешь погулять?";
+}
+
+function Fnction() {
+    document.getElementById("dm").innerHTML = "как настроение?";
+}
+
+function Funtion() {
+    document.getElementById("em").innerHTML = "еще одна кнопка и все";
+}
+
+function my() {
+    var box1 = document.getElementById("b1")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+    console.log(div.style.display);
+};
+
+function Funn() {
+    var box1 = document.getElementById("bbox")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+    console.log(div.style.display);
+};
+
+function Fnn() {
+    var box1 = document.getElementById("bgo")
+    var div = box1
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+    console.log(div.style.display);
+};
+
+
+    
